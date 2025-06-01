2f8ed090dcb5049598d3f2ffa2a5189ae26931e6
Gry4hon <griffinpenndev@gmail.com>
2025-06-01 11:29:56 -0700

    Refactor: Project box design overall rough draft

--- src/Components/ProjectsComponents/ProjectBox.tsx
+++ src/Components/ProjectsComponents/ProjectBox.tsx
@@ -1,28 +1,13 @@
 import "./styles/ProjectBoxStyles.css";
-import github from "./styles/Assets/github_logo.png";
-import logo from "./styles/Assets/LOGO.png";
+import temp from "./styles/Assets/LOGO.png";
 
-function ProjectBox(props: any) {
-  //Gotta have like a map of values for the imgs for the tech stack right?
-  //<img src="/src/Components/ProjectsComponents/styles/Assets/LOGO.png">
-  //const links = props.images;
+function ProjectBox() {
   return (
     <>
-      <div id="box-body">
-        <img className="project-picture-styles" src={logo}></img>
-        <div className="title-github">
-          <p className="title-styles">{props.title}</p>
-          <a href={props.link}>
-            <img src={github} className="githib-styles" />
-          </a>
-        </div>
-        <p className="bio-styles">{props.bio}</p>
-        {/* <p className="tech-stack-title">Tech Used:</p>
-          <div className="tech-stack-contianer">
-            {links.map((image: string) => {
-              return <img src={image} />;
-            })}
-          </div> */}
+      <div className="project-box">
+        <img src={temp} />
+        <h3>Title</h3>
+        <button id="expand-button">A</button>
       </div>
     </>
   );
